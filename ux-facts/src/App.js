78c4cab1b7fb53d2cae3d7a931cc72680c58fd08
Matt2844 <matt.hd.ford@gmail.com>
2021-03-01 23:23:51 -0500

    setup project

---
+++ ux-facts/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App () {
+  return (
+    <section className="landing-page">
+      <img className="background-img" src="/images/lamp.jpeg" />
+    </section>
+  );
+}
+
+export default App;
